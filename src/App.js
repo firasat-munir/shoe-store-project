c0d1d74b8a7aa8a05d0e5f8c918c58ac04f81778
Firasat Munir <zapp3r1997@gmail.com>
2022-09-09 18:01:00 +0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import { Header } from './components/Header';
+import { Home } from './components/Home';
+import { ProductDetail } from './components/ProductDetail';
+import { Products } from './components/Products';
+
+function NotFound(){
+  return(
+    <div>
+      <h2>Page Not Found</h2>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path='products' element={<Products />}>
+          <Route path='' element={<Products />}></Route>
+          <Route path=':productId' element={<ProductDetail />}></Route>
+        </Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Products.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Products=()=>{
+    return(
+        <div>
+            <h4>Products</h4>
+        </div>
+    )
+}
